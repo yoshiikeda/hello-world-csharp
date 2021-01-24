bd425c411ab14c582c826177fcd6c03c3cee0ad2
Yoshiyuki Ikeda <35252593+yoshiikeda@users.noreply.github.com>
2021-01-24 10:31:55 +0900

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _20210124_01
+{
+    public class Program
+    {
+        public static void Main(string[] args_)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
